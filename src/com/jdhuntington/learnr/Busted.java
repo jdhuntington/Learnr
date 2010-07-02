2d7de31ea5989e57cb56257473783bad91ba2d68
JD Huntington <jedediah@jdhuntington.com>
2010-07-01 21:16:57 -0700

    download a file and display the contents

---
+++ src/com/jdhuntington/learnr/Busted.java
@@ -0,0 +1,13 @@
+package com.jdhuntington.busted.com.jdhuntington;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Busted extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
